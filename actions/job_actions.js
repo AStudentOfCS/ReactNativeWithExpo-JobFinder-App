1cbe7951dcba64b3460ddc9c9fd2a8299a903d4f
Tom Sawyer <tomsawyer@TomSawyer.local>
2018-03-14 18:03:07 +0700

    Fulfilling additional functionalities with other Screens

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -3,7 +3,9 @@ import reverseGeocode from 'latlng-to-zip';
 import qs from 'qs';
 
 import {
-  FETCH_JOBS
+  FETCH_JOBS,
+  LIKE_JOB,
+  CLEAR_LIKED_JOBS
 } from './types';
 
 const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
@@ -34,3 +36,14 @@ export const fetchJobs = (region, callback) => async (dispatch) => {
     console.log(e);
   }
 };
+
+export const likeJob = (job) => {
+  return {
+    payload: job,
+    type: LIKE_JOB
+  };
+};
+
+export const clearLikedJobs = () => {
+  return { type: CLEAR_LIKED_JOBS };
+}
--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,18 +1,94 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
+import { connect } from 'react-redux';
+import { MapView } from 'expo';
+import { Card, Button, Icon } from 'react-native-elements';
+import Swipe from '../components/Swipe';
+import * as actions from '../actions';
 
 class DeckScreen extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Jobs',
+      tabBarIcon: ({ tintColor }) => {
+          return <Icon name="description" size={30} color={tintColor} />
+      }
+    };
+  };
+
+  renderCard (job) {
+    const initialRegion = {
+      longitude: job.longitude,
+      latitude: job.latitude,
+      longitudeDelta: 0.02,
+      latitudeDelta: 0.045
+    };
+
+    return (
+      <Card title={job.jobtitle}>
+        <View style={{ height: 300 }}>
+          <MapView
+            scrollEnabled={false}
+            style={{ flex: 1 }}
+            cacheEnabled={Platform.OS === 'android'}
+            initialRegion={initialRegion}
+          >
+
+          </MapView>
+        </View>
+
+        <View style={styles.detailWrapper}>
+          <Text>{job.company}</Text>
+          <Text>{job.formattedRelativeTime}</Text>
+        </View>
+
+        <Text style={{ height: 50 }}>
+          {job.snippet.replace(/<b>/g, '').replace(/<\/b>/g, '')}
+        </Text>
+      </Card>
+    );
+  }
+
+  renderNoMoreCards = () => {
+    return (
+      <Card title="No More Jobs">
+        <Button
+          title="Back To Map"
+          large
+          icon={{ name: 'my-location' }}
+          backgroundColor="#03A9F4"
+          onPress={() => this.props.navigation.navigate('map')}
+        />
+      </Card>
+    );
+  }
+
   render () {
     return (
-      <View>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
-        <Text>DeckScreen</Text>
+      <View style={{ marginTop: 10 }}>
+        <Swipe
+          data={this.props.jobs}
+          renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
+          onSwipeRight={job => this.props.likeJob(job)}
+          keyProp="jobkey"
+        />
       </View>
     );
   }
 }
 
-export default DeckScreen;
+const styles = {
+  detailWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 5,
+    marginTop: 5
+  }
+}
+
+const mapStateToProps = ({ jobs }) => {
+  return { jobs: jobs.results };
+}
+
+export default connect(mapStateToProps, actions)(DeckScreen);
