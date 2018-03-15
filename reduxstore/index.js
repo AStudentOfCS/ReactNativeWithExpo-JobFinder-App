8f8cb46bf2c06ffea134806aeea90e6d05e6c129
Tom Sawyer <tomsawyer@TomSawyer.local>
2018-03-15 12:43:27 +0700

    Adding feature Offline Data Persistence using redux-persist v5

--- reduxstore/index.js
+++ reduxstore/index.js
@@ -1,13 +1,23 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['likedJobs']
+}
 
 const store = createStore(
-  reducers,
+  persistReducer(persistConfig, reducers),
   {},
   compose(
     applyMiddleware(thunk)
   )
 );
+
+persistStore(store);
 
 export default store;
